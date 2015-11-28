51c2fa57de2661e4521d30f719e6e364dc3d1fb2
PahomLancer <mozggg1@gmail.com>
2015-11-28 20:19:29 +0700

    1 update fix main add massive login
    1234

--- src/auth.java
+++ src/auth.java
@@ -1,15 +1,12 @@
 public class auth{
 	int id = 0;
-	int maxid = 0;
 	public String login;
 	private String password;
 	String salt = "qa12ws34ed56rf78tg90";
-	public void setUser(String login, String password){
-		id = maxid;
+	public void setUser(int id, String login, String password){
 		this.login = login;
 		this.password = password;
-		id++;
-		maxid++;
+		this.id = id;
 	}
 	public int checkUser(auth user){
 		if (user.login.equals(this.login))
@@ -22,7 +19,7 @@ public class auth{
 		}
 		return 3;
 	}
-	public void printUser(int id)
+	public void printUser()
 	{
 		System.out.println(this.id + " " + this.login + " " + this.password);
 	}
--- src/main1.java
+++ src/main1.java
@@ -1,28 +1,43 @@
+import java.util.Scanner;
 public class main1 {
-
 	public main1() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//account ouraccount = new account();
-		//String say = ouraccount.seelogpass(1);
-		//System.out.println(say);
-		//String say2 = ouraccount.logging("Andrew", "GGGG");
-		//System.out.println(say2);
-		//hash hashaccount = new hash();
-		//System.out.println(hashaccount.makeHash("gggg", "5555"));
-		auth ouraccount1 = new auth();
-		ouraccount1.setUser("Andrew", "343421");;
-		ouraccount1.printUser(1);
-		ouraccount1.setUser("Mihail", "642422");
-		ouraccount1.printUser(1);
+        //String s1, s2;
+        //s1 = sc.nextLine();
+        //s2 = sc.nextLine();
+		Scanner sc = new Scanner(System.in);
+        int n = 0;
+        String login, password;
+        System.out.println("Enter number of accounts, that you want to add: ");
+        if(sc.hasNextInt())
+            n = sc.nextInt(); 
+        else 
+        	System.out.println("Error #1 you didn't insert a number\n");
+        String buffer = sc.nextLine();
+        auth user[] = new auth[n];
+        for (int i = 0; i < n; i++)
+            user[i] = new auth();
+        for (int i = 0; i < n; i++) {
+        	System.out.println("Insert " + i + " user login: ");
+        	login = sc.nextLine();
+        	System.out.println("Insert " + i + " user password: ");
+        	password = sc.nextLine();
+        	user[i].setUser(i, login, password);
+        	}
+        for (int i = 0; i < n; i++) {
+        	user[i].printUser();
+        	}
+		//auth ouraccount1 = new auth();
+		//ouraccount1.setUser(1, "Andrew", "343421");;
+		//ouraccount1.printUser();
+		//ouraccount1.setUser(2, "Mihail", "642422");
+		//ouraccount1.printUser();
 		//ouraccount1.getUser(ouraccount1);
-		ouraccount1.printUser(1);
-		String s1 = "H.E.X.L.E.T";
-		String s2 = "Java";
-		System.out.println(s1.toLowerCase().substring(6, 10));
+		//ouraccount1.printUser();
 		}
 	}
 
