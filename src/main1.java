b3a505890701477289ce17d1ff6a7c488cc833d0
PahomLancer <mozggg1@gmail.com>
2015-11-29 20:22:12 +0700

    2 update role add
    12345

--- src/main1.java
+++ src/main1.java
@@ -11,7 +11,7 @@ public class main1 {
         //s2 = sc.nextLine();
 		Scanner sc = new Scanner(System.in);
         int n = 0;
-        String login, password;
+        String login, password, role;
         System.out.println("Enter number of accounts, that you want to add: ");
         if(sc.hasNextInt())
             n = sc.nextInt(); 
@@ -19,17 +19,24 @@ public class main1 {
         	System.out.println("Error #1 you didn't insert a number\n");
         String buffer = sc.nextLine();
         auth user[] = new auth[n];
-        for (int i = 0; i < n; i++)
+        role roles[] = new role[n];
+        for (int i = 0; i < n; i++){
             user[i] = new auth();
+            roles[i] = new role();
+        }
         for (int i = 0; i < n; i++) {
         	System.out.println("Insert " + i + " user login: ");
         	login = sc.nextLine();
         	System.out.println("Insert " + i + " user password: ");
         	password = sc.nextLine();
         	user[i].setUser(i, login, password);
+        	System.out.println("Insert " + i + " user role (r, w, e, rw, re, we, rwe): ");
+        	role = sc.nextLine();
+        	roles[i].setRole(i, role);
         	}
         for (int i = 0; i < n; i++) {
         	user[i].printUser();
+        	System.out.println(i + " user role " + roles[i].returnvalue());
         	}
 		//auth ouraccount1 = new auth();
 		//ouraccount1.setUser(1, "Andrew", "343421");;
---
+++ src/role.java
@@ -0,0 +1,33 @@
+public class role {
+	int id;
+	//1 read 2 write 4 execute 3 read-write 5 read-execute 6 write-execute 7 read-write-execute
+	private int value;
+	public void setRole(int id, String role){
+		this.id = id;
+		switch (role) {
+            case "r": this.value = 1;
+                    break;
+            case "w": this.value = 2;
+            		break;
+            case "rw": this.value = 3;
+    				break;
+            case "e": this.value = 4;
+    				break;
+            case "re": this.value = 5;
+    				break;
+            case "we": this.value = 6;
+    				break;  
+            case "rwe": this.value = 7;
+    				break;  
+            default: this.value = 0;
+                     break;
+        }
+	}
+	public void setRole(int id, int role){
+		this.id = id;
+		this.value = role;
+	}
+	public int returnvalue(){
+		return this.value;
+	}
+}
