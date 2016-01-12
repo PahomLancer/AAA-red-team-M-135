c7a394e6ec16de27482ca0fa839913ff5b4c8895
Faust813 <oleg131513@gmail.com>
2016-01-12 18:43:31 +0700

    Create AccountingServiceю.java

---
+++ AccountingServiceю.java
@@ -0,0 +1,23 @@
+package com.blzr.service;
+
+import com.blzr.domain.Activity;
+import com.blzr.domain.Authority;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class AccountingService {
+    private static final DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    private final ConnectionService connectionService;
+
+    public AccountingService(ConnectionService connectionService) {
+        this.connectionService = connectionService;
+    }
+
+    public Long addActivity(Authority authority, String dateStart, String dateEnd, String volume) throws ParseException, NumberFormatException {
+        connectionService.addActivity(new Activity(authority,
+                format.parse(dateStart), format.parse(dateEnd), Long.valueOf(volume)));
+        return connectionService.countActivity();
+    }
+}
