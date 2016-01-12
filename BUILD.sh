5d6e65c273b290af8dff2b297eb171f7d9312f78
PahomLancer <mozggg1@gmail.com>
2016-01-12 18:20:35 +0700

    Чистка под R3

--- BUILD.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -v
-rm -rf "out"
-mkdir -p "out/classes"
-find . -name "*.java" | xargs javac -cp "lib/*" -d out/classes -sourcepath src -verbose
-
-mkdir -p "out/lib"
-cp lib/* out/lib/
-
-jar -cfe out/aaa.jar com.andr.Main -C out/classes/ .
--- RUN.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# add -x for debug output
-java -cp "lib/*:out/aaa.jar" com.andr.Main $@ > /dev/null
