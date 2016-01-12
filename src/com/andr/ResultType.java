754b5d46ef3eef1b57a1f62124d2d070084220e8
PahomLancer <mozggg1@gmail.com>
2016-01-12 16:50:36 +0700

    Перенес файлы, востановил иерархию

---
+++ src/com/andr/ResultType.java
@@ -0,0 +1,20 @@
+package com.blzr;
+
+public enum ResultType {
+    SUCCESS(0),
+    UNKNOWN_LOGIN(1),
+    INVALID_PASSWORD(2),
+    UNKNOWN_ROLE(3),
+    ACCESS_DENIED(4),
+    INVALID_ACTIVITY(5);
+
+    private final int code;
+
+    ResultType(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
