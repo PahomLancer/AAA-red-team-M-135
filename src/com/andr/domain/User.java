d91bc369a6fd1cb1c6e7e774bfadf7a1c6247576
kryksh <nice.candy.lol@gmail.com>
2016-01-12 23:53:33 +0700

    Update User.java

--- src/com/andr/domain/User.java
+++ src/com/andr/domain/User.java
@@ -3,6 +3,7 @@ package com.andr.domain;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
 
+//opredelenie klassa User
 public class User {
     private Long id;
     private String name;
@@ -18,50 +19,62 @@ public class User {
         this.hash = generateHash(password, this.salt);
     }
 
+    //return ID
     public Long getId() {
         return id;
     }
 
+    //zadat ID
     public void setId(Long id) {
         this.id = id;
     }
-
+    
+    //return name
     public String getName() {
         return name;
     }
-
+    
+    //zadat name
     public void setName(String name) {
         this.name = name;
     }
-
+    
+    //return login
     public String getLogin() {
         return login;
     }
-
+    
+    //zadat login
     public void setLogin(String login) {
         this.login = login;
     }
-
+    
+    //return hash
     public byte[] getHash() {
         return hash;
     }
-
+    
+    //zadat hash
     public void setHash(byte[] hash) {
         this.hash = hash;
     }
-
+    
+    //return sole
     public byte[] getSalt() {
         return salt;
     }
 
+    //zadat sole
     public void setSalt(byte[] salt) {
         this.salt = salt;
     }
 
+    //proverka parolya na validnost
     public boolean validatePassword(String password) {
         return MessageDigest.isEqual(generateHash(password, this.salt), this.getHash());
     }
-
+    
+    //generaciya hasha
     public static byte[] generateHash(String password, byte[] salt) {
         try {
             MessageDigest sha = MessageDigest.getInstance("SHA-256");
@@ -76,6 +89,7 @@ public class User {
         }
     }
 
+    //generaciya soli
     private static byte[] generateSalt() {
         SecureRandom random = new SecureRandom();
         byte[] bytes = new byte[32];
