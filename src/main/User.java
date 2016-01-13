e8f631caed5961a7c773c69de7f0ea247890129a
kryksh <nice.candy.lol@gmail.com>
2016-01-13 18:44:18 +0700

    Update and rename user.java to User.java

--- src/main/User.java
+++ src/main/User.java
@@ -1,20 +1,20 @@
 package main;
 
-public class user {
+public class User {
     private int id;
     private String name;
     private String password;
     private String login;
     private String salt;
-    //Задаем пользователя
-    public user(int id, String name, String password, String login, String salt) {
+
+    public User(int id, String name, String password, String login, String salt) {
         this.id = id;
         this.name = name;
         this.password = password;
         this.login = login;
         this.salt = salt;
     }
-    //Функции возвращающие инкапсулированные переменные
+
     public int returnId() {
         return id;
     }
