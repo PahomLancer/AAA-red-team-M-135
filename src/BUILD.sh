a096176a4f05e1617b6d553b99c0a2cf60c4342d
kryksh <nice.candy.lol@gmail.com>
2016-01-13 15:58:01 +0700

    Delete BUILD.sh

--- src/BUILD.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-mkdir -p workAP 
-javac -classpath ".:../libs/*" -sourcepath EnumConstants -d ../bin EnumConstants/*.java
-javac -classpath ".:../libs/*" -sourcepath . -d ../bin *.java
-jar -cfe AAA.jar main1 -C workAP .
