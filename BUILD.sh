7a31aadcbc10c0e197759a79a6de976c2aea9e84
PahomLancer <mozggg1@gmail.com>
2016-01-14 01:17:27 +0700

    Update BUILD.sh

--- BUILD.sh
+++ BUILD.sh
@@ -2,9 +2,7 @@
 rm -rf "aaa"
 mkdir -p aaa/classes 
 find . -name "*.java" | xargs javac -cp "lib/*" -d aaa/classes -sourcepath src -verbose
-#find . -name "*.java" | xargs javac -cp "lib/*" -d aaa -sourcepath src -verbose
 
-#cp -r src/ aaa/classes/
 cp -r resources/ aaa/classes/
 #cp -r resources/ aaa/
 
@@ -12,4 +10,3 @@ mkdir -p "aaa/lib"
 cp lib/* aaa/lib/
 
 jar -cfe aaa/aaa.jar Main -C aaa/classes/ .
-#jar -cfe aaa/aaa.jar Main -C aaa/ .
