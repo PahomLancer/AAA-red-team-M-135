6487b6666b8a194134ec928f1b83d979680ba68b
PahomLancer <mozggg1@gmail.com>
2016-01-13 23:13:06 +0700

    Перенос скриптов из р2

---
+++ BUILD.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -v
+rm -rf "workAP"
+mkdir -p workAP/classes 
+find . -name "*.java" | xargs javac -cp "lib/*" -d workAP/classes -sourcepath src -verbose
+
+cp -r src/account/ workAP/classes/
+cp -r src/auth/ workAP/classes/
+cp -r src/role/ workAP/classes/
+cp -r src/work/ workAP/classes/
+
+mkdir -p "workAP/lib"
+cp lib/* workAP/lib/
+
+jar -cfe workAP/aaa.jar Main -C workAP/classes/ .
