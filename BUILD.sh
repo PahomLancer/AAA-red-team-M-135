f0ade4e627caff3a3f491dee695f67d49e7ed8bd
kryksh <nice.candy.lol@gmail.com>
2016-01-13 23:39:13 +0700

    Update BUILD.sh

--- BUILD.sh
+++ BUILD.sh
@@ -4,6 +4,7 @@ mkdir -p workAP/classes
 find . -name "*.java" | xargs javac -cp "lib/*" -d workAP/classes -sourcepath src -verbose
 
 cp -r src/ workAP/classes/
+cp -r src/resources/ workAP/classes/
 
 mkdir -p "workAP/lib"
 cp lib/* workAP/lib/
