9ba2ffa915e60be667c00dc0b413fcd96d665c0b
PahomLancer <mozggg1@gmail.com>
2016-01-14 17:05:17 +0700

    Update BUILD.sh

--- BUILD.sh
+++ BUILD.sh
@@ -3,6 +3,7 @@ rm -rf "aaa"
 mkdir -p aaa/classes 
 find . -name "*.java" | xargs javac -cp "lib/*" -d aaa/classes -sourcepath src -verbose
 
+cp -r src/* aaa/classes/
 cp -r resources/* aaa/classes/
 #cp -r resources/ aaa/
 
