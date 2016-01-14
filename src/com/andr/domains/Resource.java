091622507738a2f412e4ccaa90b585c8d76576ed
PahomLancer <mozggg1@gmail.com>
2016-01-14 18:49:22 +0700

    Update Resource.java

--- src/com/andr/domains/Resource.java
+++ src/com/andr/domains/Resource.java
@@ -1,23 +1,29 @@
 package com.andr.domains;
 
 public class Resource {
+
     private int id;
+
     private String name;
+
     private Resource id_parent;
-    //Set recoursess
+
     public Resource(int id, String name, Resource id_parent) {
         this.id = id;
         this.name = name;
         this.id_parent = id_parent;
     }
-    //Get perem
-    public int returnId() {
+
+    public int getId() {
         return id;
     }
-    public String returnName() {
+
+    public String getName() {
         return name;
     }
-    public Resource returnId_parent() {
+
+    public Resource getId_parent() {
         return id_parent;
     }
+
 }
